afd87766cb26f1a57aea469e1f006cc8fe50da98
Carolina Lopes <ccal2@cin.ufpe.br>
2018-11-15 17:24:36 -0300

    DNS for multiple servers

--- modulos/DNS.py
+++ modulos/DNS.py
@@ -4,7 +4,7 @@ import socket
 HOST = ''
 PORT = 49152 # o número de porta pode ser entre 49152 e 65535
 
-server = None
+servers = {}
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((HOST, PORT))
@@ -19,19 +19,19 @@ while True:
     if code == '0':
         server_domain = msg # deixa salvo dominio do servidor
         server_ip = addr[0] # deixa salvo ip do servidor
-        
-        server = (server_domain, server_ip)
+    
+        servers[server_domain] = server_ip
         
         if not data: break
         s.sendto(data, addr) # entrega pro cliente (servidor) confirmando
 
-        print('server', addr, 'with domain', server[0], 'has been saved')
+        print('server', addr, 'with domain', msg, 'has been saved')
     elif code == '1':
-        if server and msg == server[0]:
+        if servers and msg in servers:
             if not data: break
-            s.sendto(server[1].encode(), addr) # send server's IP to client
+            s.sendto(servers[msg].encode(), addr) # send server's IP to client
 
-            print('IP address', server[1], 'for domain', server[0], 'has been sent')
+            print('IP address', servers[msg], 'for domain', msg, 'has been sent')
         else:
             if not data: break
             s.sendto(b'-1', addr) # send error code
