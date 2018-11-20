9cfaa65d44a5781b64e8ca497b366fa53ee6c221
Carolina Lopes <ccal2@cin.ufpe.br>
2018-11-20 16:00:17 -0300

    Server: send files list | Client: choose file

--- modulos/client.py
+++ modulos/client.py
@@ -1,8 +1,12 @@
 import socket
 from MySocket import MySocket
+import pickle
 
 DNS_HOST = ''
 DNS_PORT = 49152
+
+server_PORT = 50000
+SIZE = 1024     # ver isso!!
 
 # send server's domain to DNS and get its IP
 with MySocket(socket.AF_INET, socket.SOCK_DGRAM) as s:
@@ -12,4 +16,25 @@ with MySocket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     if data.decode() == '-1':
         print('domain not found')
     else:
+        server_HOST = data
         print('Received', data.decode())
+
+
+# connect to server
+socket_client = MySocket(socket.AF_INET, socket.SOCK_STREAM)
+socket_client.connect((server_HOST, server_PORT))
+
+d_files = socket_client.recv(SIZE*30)    # ajeitar tamanho!!
+d_files = pickle.loads(d_files) # transform data in list
+
+# print file_names
+for i, file_name in enumerate(d_files):
+    print(i, '-', file_name)
+
+msg = input('Digite o número do arquivo desejado: ')
+socket_client.send(msg.encode())
+
+resposta = socket_client.recv(SIZE)
+print(resposta.decode())
+
+socket_client.close()
--- modulos/server.py
+++ modulos/server.py
@@ -1,11 +1,42 @@
 import socket
 from MySocket import MySocket
+from os import listdir
+import pickle
 
 DNS_HOST = ''
 DNS_PORT = 49152 # o numero de porta pode ser entre 49152 e 65535
+
+self_HOST = ''
+self_PORT = 50000
+SIZE = 1024     # ver isso!!
 
 # send IP and domain to DNS
 with MySocket(socket.AF_INET, socket.SOCK_DGRAM) as s:
     s.dnsRegisterDomain("bois.com", DNS_HOST, DNS_PORT)
     data = s.recv(1024)
     print('Received', data.decode())
+
+
+# wait for clients
+server_socket = MySocket(socket.AF_INET, socket.SOCK_STREAM)
+
+server_socket.bind((self_HOST, self_PORT))
+server_socket.listen(1)
+while True:
+    conn, addr = server_socket.accept()
+    print('Conectado com:', addr)
+
+    files = listdir('../arquivos')  # list all files at the folder 'arquivos'
+    d_files = pickle.dumps(files)   # serialize files
+    conn.send(d_files)              # send files' list to client
+
+    while conn:
+        dados = conn.recv(SIZE)     # receive data from client
+        if not dados: break
+        conn.send(b'Eu sou servidor. Voce disse: ' + dados)
+
+        choosen_file = int(dados.decode())  # transform client's data in int
+
+        print('arquivo escolhido:', files[choosen_file])
+
+    conn.close()
