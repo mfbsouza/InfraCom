63dd4ad5bbf600779ac98624f7c6a2be8b981417
Carolina Lopes <ccal2@cin.ufpe.br>
2018-12-16 16:19:40 -0300

    Minor adjusts

--- modulos/udp_socket.py
+++ modulos/udp_socket.py
@@ -76,7 +76,6 @@ class SocketUDP:
         # send fin_msg and break send_loop
         fin_1_msg = Package(self.__seq, self.__other_seq, False, False, True, is_fin_1=True)
         self.__send_buffer.put(fin_1_msg)
-        print('send fin_1')
 
         print('Closing connection...')
 
@@ -128,10 +127,10 @@ class SocketUDP:
                 raise Exception(str(to_addr) + " is not responding :/")
             
             if package.is_fin_1:
-                print('sent fin_1, break send_loop client')
+                # print('sent fin_1, break send_loop client')
                 break
             if close_connection:
-                print('received ack for fin_2, break send_loop server')
+                # print('received ack for fin_2, break send_loop server')
                 break
 
     @staticmethod
@@ -144,7 +143,7 @@ class SocketUDP:
 
                 # break loop on server
                 if package.is_fin_2:
-                    print('received ack for fin_2, break recv_loop server')
+                    # print('received ack for fin_2, break recv_loop server')
                     break
             elif package.is_syn:
                 sock.sendto(Package(0, package.seq + 1, True, False, False).encode(), to_addr)
@@ -153,7 +152,7 @@ class SocketUDP:
                 sock.sendto(Package(0, package.seq + 1, True, False, False).encode(), to_addr)
 
                 # send fin_2
-                print('received fin_1, sending fin_2')
+                # print('received fin_1, sending fin_2')
                 fin_2_msg = Package(0, package.seq + 1, False, False, True, is_fin_2=True)
                 send_buffer.put(fin_2_msg)
             elif package.is_fin_2:
@@ -161,7 +160,7 @@ class SocketUDP:
                 sock.sendto(Package(0, package.seq + 1, True, False, False, is_fin_2=True).encode(), to_addr)
 
                 # break loop on client
-                print('received fin_2, break recv_loop client')
+                # print('received fin_2, break recv_loop client')
                 break
             else:
                 recv_buffer.put(package)
